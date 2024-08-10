355885307fa8c65cb40156913331ced8e8c8a1be
Luiz Filipe Seabra de marco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2024-08-10 13:42:03 -0300

    Ultima atualização no README

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as arq:
     readme = arq.read()
 
 setup(name='nano_wait',
-    version='0.0.1',
+    version='0.0.3',
     license='MIT License',
     author='Luiz Filipe Seabra de Marco',
     long_description=readme,
